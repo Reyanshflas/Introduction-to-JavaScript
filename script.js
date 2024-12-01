ba26c9bb41db3562714ca8d4f994ac0ab07d3157
Reyanshflas <reyanshflasplayz@gmail.com>
2024-12-01 10:15:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("js").innerText="This is my first Js code"
+
+var a = 15
+var b = 100
+var sum = a+b
+document.getElementById("sum").innerText = "Addition: " + sum
+
+var c = 50
+var d = 51
+var subtract = d-c
+document.getElementById("subtract").innerText = "Subtraction: " + subtract
+
+var x = 5
+var y = 10
+var mul = x*y
+document.getElementById("multiply").innerText = "Multiplication: " + mul
+
+var r = 89
+var s = 6
+var div = r/s
+document.getElementById("divide").innerText = "Division: "+div
+
+let f = 5
+document.getElementById("same_number").innerText = a
+a*=a
+document.getElementById("same_number").innerText = a
